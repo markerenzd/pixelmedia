f982bbac9ffa60d73d92507ac69ce0622600de3d
Markd <mark@qstrike.com>
2018-05-29 14:30:17 +0800

    added form

--- wp-content/themes/pixelmedia/index.php
+++ wp-content/themes/pixelmedia/index.php
@@ -61,11 +61,5 @@
   data-remodal-options="hashTracking: false, closeOnOutsideClick: false">
 
   <button data-remodal-action="close" class="remodal-close"></button>
-  <h1>Book Now</h1>
-  <p>
-    Responsive, lightweight, fast, synchronized with CSS animations, fully customizable modal window plugin with declarative configuration and hash tracking.
-  </p>
-  <br>
-  <button data-remodal-action="cancel" class="remodal-cancel">Cancel</button>
-  <button data-remodal-action="confirm" class="remodal-confirm">OK</button>
+<?php echo do_shortcode('[contact-form-7 id="28" title="Main Contact Form"]') ?>
 </div>
